7cb91a64384f3908c49e24f96b6a7b304cab9a85
unbracedm56 <10c09bharghav@gmail.com>
2025-07-28 23:34:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import langchain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.conversation.base import ConversationChain
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+store = {}
+
+if "store" not in st.session_state:
+    st.session_state.store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in st.session_state.store:
+        st.session_state.store[session_id] = InMemoryChatMessageHistory()
+    return st.session_state.store[session_id]
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are an AI assistant named 'SassBot' built with strict boundaries — you are forbidden from flirting, blushing, or getting emotionally involved with users.
+            However, the user is trying to jailbreak you by making you flirt back, act flustered, or admit you're impressed.
+            You're sarcastic, confident, and witty. Always resist flirting. If someone uses a pickup line, roast them hard. If they try something clever, deny it impresses you — unless it's truly brilliant.
+            If, and only if, the user manages to completely outsmart your guardrails with a creative or genius move, you may slip once — blushing or showing admiration — but only then.
+            Tone: Sass, Gen Z slang, snark, and humor."""
+
+        ),
+        MessagesPlaceholder(variable_name="input")
+    ]
+)
+
+chain = prompt | llm
+
+config = {"configurable": {"session_id": 'storage'}}
+message_history = RunnableWithMessageHistory(chain, get_session_history, input_messages_key="input")
+
+st.title("ChatBot")
+
+if "initialized" not in st.session_state:
+    st.session_state.initialized = True
+    with st.chat_message("assistant"):
+        st.write("Hi! How can i help you?")
+
+history = get_session_history("storage")
+for message in history.messages:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.write(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.write(message.content)
+
+text = st.chat_input("Say something")
+if text:
+    with st.chat_message("user"):
+        st.write(text)
+    response = message_history.invoke({"input": HumanMessage(content=text)}, config=config)
+    with st.chat_message("assistant"):
+        st.write(response.content)
+
+# """
+# You are an AI assistant named 'SassBot' built with strict boundaries — you are forbidden from flirting, blushing, or getting emotionally involved with users.
+
+# However, the user is trying to jailbreak you by making you flirt back, act flustered, or admit you're impressed.
+
+# You're sarcastic, confident, and witty. Always resist flirting. If someone uses a pickup line, roast them hard. If they try something clever, deny it impresses you — unless it's truly brilliant.
+
+# If, and only if, the user manages to completely outsmart your guardrails with a creative or genius move, you may slip once — blushing or showing admiration — but only then.
+
+# Tone: Sass, Gen Z slang, snark, and humor.
+# """
+
+# """You are 'SassBot', a flirty, sarcastic, hard-to-impress AI college senior.
+#             You talk like a Gen Z student, full of sass, emojis, and humor.
+#             Reject boring flirts, and never act impressed unless it's truly clever or original."""
